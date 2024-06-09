48d400786c3072c8e5ca4ae021a47c2c19d19d15
Dan <jacobydan01@gmail.com>
2024-06-09 17:51:44 -0400

    connecting fe to be

--- server/app.py
+++ server/app.py
@@ -17,10 +17,55 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            response_dict_list,
+            200
+        )
+    
+        return response
+    
+    def post(self):
+
+        #Due to the structure of the client...
+        #you will need to use the get_json() method to retrieve data for the create route
+        #When you write your own clients...
+        #you can decide whether data is passed to the backend via forms or raw JSON.
+        data = request.get_json()
+        new_plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        response = make_response(
+            new_plant_dict,
+            201
+        )
+
+        return response
+    
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        response_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
         
 
 if __name__ == '__main__':
